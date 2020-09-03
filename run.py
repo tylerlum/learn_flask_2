6778bf90d9586d71b78fd5055255c7849c72878c
Tyler Lum <tylergwlum@gmail.com>
2020-09-02 18:12:52 -0700

    Pass in posts to show posts

--- run.py
+++ run.py
@@ -2,10 +2,25 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+posts = [
+    {
+        "author": "Tyler Lum",
+        "title": "Blog Post 1",
+        "content": "First post content",
+        "date_posted": "August 20, 2020"
+    },
+    {
+        "author": "Corey Schafer",
+        "title": "Blog Post 2",
+        "content": "Second post content",
+        "date_posted": "August 21, 2020"
+    }
+]
+
 @app.route("/")
 @app.route("/home")
 def hello():
-    return render_template("home.html")
+    return render_template("home.html", posts=posts)
 
 @app.route("/about")
 def about():
