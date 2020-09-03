91ae112b99d247db29ed9054bb1d12832b4d6323
Tyler Lum <tylergwlum@gmail.com>
2020-09-03 15:01:23 -0700

    Add flash account created messages

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
@@ -21,16 +21,19 @@ posts = [
 
 @app.route("/")
 @app.route("/home")
-def hello():
+def home():
     return render_template("home.html", posts=posts, title="My Title")
 
 @app.route("/about")
 def about():
     return render_template("about.html")
 
-@app.route("/register")
+@app.route("/register", methods=["GET", "POST"])
 def register():
     form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f"Account created for {form.username.data}!", "success")
+        return redirect(url_for('home'))
     return render_template("register.html", title="Register", form=form)
 
 @app.route("/login")
